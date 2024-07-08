a5eb365acd8ad7cd7bf75cba02b69786acf22136
Vasudevan <vasudevan.s@outlook.com>
2024-07-07 21:23:16 -0700

    Updated TestDataFramework
    Added comment

--- src/main/java/pro/vasudevan/testdataframework/interfaces/TestData.java
+++ src/main/java/pro/vasudevan/testdataframework/interfaces/TestData.java
@@ -6,6 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ *
+ * Created By: Vasudevan Sampath
+ * 
  * TestData.java - custom annotation interface
  * Requires a valid CSV file (virtual path)
  * rowDataDelimiter parameter is optional and if not set, defaults to ","
